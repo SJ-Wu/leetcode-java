ba9d4a521b54f62bba1b308237bcfbace1ba0afa
ShengJu <nickwu0301@gmail.com>
2023-11-22 13:00:02 +0800

    leetcode(217): add contains duplicate example tests

---
+++ src/main/java/neetcode/arrays_hashing/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+package neetcode.arrays_hashing;
+
+public class ContainsDuplicate {
+    Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        var containsDuplicate = new ContainsDuplicate();
+        System.out.println(containsDuplicate.solution.containsDuplicate(new int[]{1, 2, 3, 1}));
+    }
+
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/neetcode/arrays_hashing/ContainsDuplicateTest.java
@@ -0,0 +1,28 @@
+package neetcode.arrays_hashing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ContainsDuplicateTest {
+
+    @Test
+    void example1() {
+        var containsDuplicate = new ContainsDuplicate();
+        assertThat(containsDuplicate.solution.containsDuplicate(new int[]{1, 2, 3, 1})).isEqualTo(true);
+    }
+
+    @Test
+    void example2() {
+        var containsDuplicate = new ContainsDuplicate();
+        assertThat(containsDuplicate.solution.containsDuplicate(new int[]{1, 2, 3, 4})).isEqualTo(false);
+    }
+
+    @Test
+    void example3() {
+        var containsDuplicate = new ContainsDuplicate();
+        assertThat(containsDuplicate.solution.containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})).isEqualTo(true);
+    }
+
+
+}
