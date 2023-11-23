6093d425668399d31a9d454829722f8517e69bc0
ShengJu <nickwu0301@gmail.com>
2023-11-23 12:42:54 +0800

    leetcode(36): add valid sudoku test example tests

---
+++ src/main/java/neetcode/arrays_hashing/ValidSudoku.java
@@ -0,0 +1,7 @@
+package neetcode.arrays_hashing;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        return false;
+    }
+}
---
+++ src/test/java/neetcode/arrays_hashing/ValidSudokuTest.java
@@ -0,0 +1,25 @@
+package neetcode.arrays_hashing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidSudokuTest {
+    char[][] board = new char[][]{
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+    };
+
+    @Test
+    void example1() {
+        var solution = new ValidSudoku();
+        assertThat(solution.isValidSudoku(board)).isEqualTo(true);
+    }
+}
