c974a0a3d28e015699b2d445b5da23f88813fdad
ShengJu <nickwu0301@gmail.com>
2023-11-25 01:56:27 +0800

    feat: update solutions.

--- src/main/java/neetcode/arrays_hashing/ProductOfArrayExceptSelf.java
+++ src/main/java/neetcode/arrays_hashing/ProductOfArrayExceptSelf.java
@@ -2,6 +2,17 @@ package neetcode.arrays_hashing;
 
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-        return new int[]{};
+        var products = new int[nums.length];
+        var prefix = 1;
+        for (int i = 0; i < nums.length; i++) {
+            products[i] = prefix;
+            prefix *= nums[i];
+        }
+        var postfix = 1;
+        for (int i = nums.length-1; i >=0; i--) {
+            products[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return products;
     }
 }
--- src/main/java/neetcode/arrays_hashing/ValidSudoku.java
+++ src/main/java/neetcode/arrays_hashing/ValidSudoku.java
@@ -1,7 +1,38 @@
 package neetcode.arrays_hashing;
 
+import java.util.*;
+
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
-        return false;
+        List<Set<Character>> cols = new ArrayList<>();
+        List<Set<Character>> rows = new ArrayList<>();
+        List<Set<Character>> boxes = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            cols.add(new HashSet<>());
+            rows.add(new HashSet<>());
+            boxes.add(new HashSet<>());
+        }
+        for (int c = 0; c < 9; c++) {
+            for (int r = 0; r < 9; r++) {
+                var n = board[c][r];
+                if (n == '.'){
+                    continue;
+                }
+                if (cols.get(r).contains(n) ||
+                    rows.get(c).contains(n) ||
+                    boxes.get(getBoxIndex(c,r)).contains(n)) {
+                    return false;
+                }
+                cols.get(r).add(n);
+                rows.get(c).add(n);
+                boxes.get(getBoxIndex(c,r)).add(n);
+            }
+        }
+        return true;
+    }
+
+    private int getBoxIndex(int c, int r) {
+        return (c/3)*3+r/3;
     }
 }
