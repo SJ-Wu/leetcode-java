9d916ca4e7f6e613538f56dc6a17a78c85a85220
ShengJu <nickwu0301@gmail.com>
2023-11-28 10:31:17 +0800

    leetcode(128): add longest consecutive sequence tests.

---
+++ src/main/java/neetcode/arrays_hashing/LongestConsecutiveSequence.java
@@ -0,0 +1,12 @@
+package neetcode.arrays_hashing;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        var nums = new int[]{100,4,200,1,3,2};
+        var solution = new LongestConsecutiveSequence();
+        System.out.println(solution.longestConsecutive(nums));
+    }
+    public int longestConsecutive(int[] nums) {
+        return -1;
+    }
+}
